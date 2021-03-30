0fcd6bb2149c55ae96d3930ddddf38336adc82f8
Abrar Barbhuyia <abrar@Abrars-MacBook-Pro.local>
2021-03-30 14:44:26 +1100

    added all possible test cases to the skeleton of test.py

--- eResturant_web/app/tests.py
+++ eResturant_web/app/tests.py
@@ -3,7 +3,37 @@ from django.test import TestCase
 # Create your tests here.
 def test_register():
     try:
-        (1 == 1)
+        assert(1 == 2), "Registration Failed"
+    except ValueError as e:
+        print(e)
+
+def test_login():
+    try:
+        assert(1 == 2), "Login failed"
+    except ValueError as e:
+        print(e)
+
+def test_logout():
+    try:
+        assert(1 == 2), "Logout faield"
+    except ValueError as e:
+        print(e)
+
+def test_tableBook():
+    try:
+        assert(1 == 2), "Table booking failed"
+    except ValueError as e:
+        print(e)
+
+def test_tableEdit():
+    try:
+        assert(1 == 2), "Table booking edit failed"
+    except ValueError as e:
+        print(e)
+
+def test_tableCancel():
+    try:
+        assert(1 == 2), "Table cancelling failed"
     except ValueError as e:
         print(e)
 
