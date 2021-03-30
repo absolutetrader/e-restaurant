0bb6768c2928a7591f21cc3f0a5d6651c28383ea
Abrar Barbhuyia <abrar@Abrars-MacBook-Pro.local>
2021-03-30 14:37:27 +1100

    made skeleton for testing, just simple example test case for now

--- eResturant_web/app/tests.py
+++ eResturant_web/app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+def test_register():
+    try:
+        (1 == 1)
+    except ValueError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    test_register()
+    print("All tests passed")
