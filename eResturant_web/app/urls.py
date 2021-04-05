a47eb54942a694e10929de94c669510c958614d4
Abrar Barbhuyia <abrarbarbhuyia@192-168-1-125.tpgi.com.au>
2021-04-06 02:09:06 +1000

    made html template for easier integration

--- eResturant_web/app/urls.py
+++ eResturant_web/app/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("hello/<name>", views.hello_there, name="hello_there"),
+    path("hello/<name>", views.home, name="hello_there"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- eResturant_web/app/views.py
+++ eResturant_web/app/views.py
@@ -6,14 +6,4 @@ from django.utils.timezone import datetime
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, Django!")
-
-def hello_there(request, name):
-    return render(
-        request,
-        'hello_there.html',
-        {
-            'name': name,
-            'date': datetime.now()
-        }
-    )
+    return render(request, 'home.html', {})
