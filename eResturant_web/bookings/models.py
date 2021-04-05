682199425a2ac88c1956eef62bc9fe5e44b4ea21
Abrar Barbhuyia <abrarbarbhuyia@192-168-1-125.tpgi.com.au>
2021-04-05 22:32:18 +1000

    edited the bookings app

--- eResturant_web/bookings/models.py
+++ eResturant_web/bookings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Table(models.Model):
+    maxCapacity = models.IntegerField()
+
+class Booking(models.Model):
+    booking_start_time = models.DateTimeField()
+    booking_end_time = models.DateTimeField()
--- eResturant_web/eResturant_web/settings.py
+++ eResturant_web/eResturant_web/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My own apps
     'app',
+    'bookings'
 ]
 
 MIDDLEWARE = [
