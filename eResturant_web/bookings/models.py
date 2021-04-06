7c67596be110fb922808c7f1e8a024d0ec3bc0cf
Angus Kerr <anguskerr02@gmail.com>
2021-04-06 14:42:57 +1000

    Fixed data fields for table booking(Not sure if my commit is broken)

--- eResturant_web/bookings/models.py
+++ eResturant_web/bookings/models.py
@@ -5,7 +5,10 @@ class Table(models.Model):
     maxCapacity = models.IntegerField()
     isBooked = models.BooleanField(default='false')
 
+class DateTime(models.Model):
+    booking_start_time = models.DateTimeField()
+    booking_end_time = models.DateTimeField()
+
 class Booking(models.Model):
-    booking_start_time = models.DateTimeField(primary_key=True)
-    booking_end_time = models.DateTimeField(primary_key=True)
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    dateTime = models.ForeignKey(DateTime, on_delete=models.CASCADE)
