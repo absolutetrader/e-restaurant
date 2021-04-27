4b0c54511f933d8f1c0c3d665050ad726f2e6912
Ethan Burgess <etho.burgess@gmail.com>
2021-04-27 14:26:10 +1000

    fully merged

--- eResturant_web/bookings/models.py
+++ eResturant_web/bookings/models.py
@@ -11,8 +11,8 @@ class Table(models.Model):
 
 class Booking(models.Model):
     user = models.ForeignKey(
-        'app.User', on_delete=models.CASCADE, default=1)
-    table = models.ForeignKey(Table, on_delete=models.CASCADE, default= "")
+        'app.userProfile', on_delete=models.CASCADE, default=1)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE, default="")
     bookingStartDateTime = models.DateTimeField(default=datetime.now)
     bookingEndDateTime = models.DateTimeField(default=datetime.now)
     guests = models.IntegerField(default=1)
