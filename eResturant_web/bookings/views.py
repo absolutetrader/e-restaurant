0c275de1290486102195a9ad4800a0d8fc239e95
Ethan Burgess <etho.burgess@gmail.com>
2021-04-27 14:07:17 +1000

    maintanence

--- eResturant_web/bookings/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def bookings_view(request, *args, **kwargs):
-    print(request.user)
-    #return HttpResponse("<h1>I'm meant to be a booking page</h1>")
-    return render(request, 'bookings.html', {})
