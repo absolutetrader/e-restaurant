6ff1afb78e8df637caec2e43c5c7122a72aa9d9c
Angus Kerr <anguskerr02@gmail.com>
2021-05-07 21:58:20 +1000

    Implemented viewing booking and additional pages required for the booking process

--- eResturant_web/app/forms/booking_forms.py
+++ eResturant_web/app/forms/booking_forms.py
@@ -4,6 +4,38 @@ from app.models.booking_model import Table, Booking
 from django.forms import ValidationError
 from datetime import datetime, timedelta
 
+def get_available_tables(booking_start_date_time, booking_end_date_time, guests):
+    l_bound_time = booking_start_date_time
+    u_bound_time = booking_end_date_time
+
+    tables_booked_ids = []
+    # Exclude tables which start and end booking date includes requested initial booking date_time
+    tables_booked = Booking.objects.filter(bookingStartDateTime__lt= l_bound_time, bookingEndDateTime__gt = l_bound_time).values('table')
+    tables_booked_ids_temp = [x['table'] for x in tables_booked]
+    tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
+    #print([x for x in tables_booked_ids])
+    # Exclude tables which start and end booking date includes requested ending booking date_time
+    tables_booked = Booking.objects.filter(bookingStartDateTime__lt = u_bound_time, bookingEndDateTime__gt = u_bound_time).values('table')
+    tables_booked_ids_temp = [x['table'] for x in tables_booked]
+    tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
+
+    # Exclude tables which booking slots is inside requested booking slot
+    #tables_booked = Booking.objects.filter(bookingStartDateTime__gt = l_bound_time, bookingEndDateTime__lt = u_bound_time).values('table')
+    #tables_booked_ids_temp = [x['table'] for x in tables_booked]
+    #tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
+
+    # Exclude tables which include requested booking slot
+    #tables_booked = Booking.objects.filter(bookingStartDateTime__lt = l_bound_time, bookingEndDateTime__gt = u_bound_time).values('table')
+    #tables_booked_ids_temp = [x['table'] for x in tables_booked]
+    #tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
+
+    # Then I get a list of all the tables, of the needed size, available in that restaurant and
+    # I exclude the previous list of unavailable tables. I order the list from the smaller table
+    # to the bigger one and I return the first, smaller one, available.
+    tables = Table.objects.filter(maxCapacity__gte=guests).exclude(id__in=tables_booked_ids)
+
+    return tables
+
 
 class DateTimeInput(forms.DateTimeInput):
     input_type = 'datetime-local'
@@ -41,50 +73,29 @@ class InitialBookingForm(forms.ModelForm):
     
 
 class FinalBookingForm(forms.Form):  
-    
-    table = forms.ModelChoiceField(queryset = Table.objects.all())
-
+    booking = Booking.objects.all().last()
+    if(booking is None):
+        table = forms.ModelChoiceField(queryset = Table.objects.all(), widget=forms.RadioSelect())
+    else:
+        table = forms.ModelChoiceField(queryset = get_available_tables(booking.bookingStartDateTime, booking.bookingEndDateTime, booking.guests), widget=forms.RadioSelect())
 
 class editBookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = [
-            'id',
             'guests',
             'table',
             'bookingStartDateTime',
             'bookingEndDateTime'
         ]
+        widgets = {
+            'bookingStartDateTime' : DateTimeInput(),
+            'bookingEndDateTime' : DateTimeInput(),
+        }
+        labels = {
+            'guests' : 'Number of Guests:',
+            'bookingStartDateTime' : 'Booking Start Time',
+            'bookingEndDateTime' : 'Booking End Time',
+        }
 
 
-def get_available_tables(booking_start_date_time, booking_end_date_time, guests):
-    l_bound_time = booking_start_date_time
-    u_bound_time = booking_end_date_time
-
-    tables_booked_ids = []
-    # Exclude tables which start and end booking date includes requested initial booking date_time
-    tables_booked = Booking.objects.filter(bookingStartDateTime__lt= l_bound_time, bookingEndDateTime__gt = l_bound_time).values('table')
-    tables_booked_ids_temp = [x['table'] for x in tables_booked]
-    tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
-
-    # Exclude tables which start and end booking date includes requested ending booking date_time
-    tables_booked = Booking.objects.filter(bookingStartDateTime__lt = u_bound_time, bookingEndDateTime__gt = u_bound_time).values('table')
-    tables_booked_ids_temp = [x['table'] for x in tables_booked]
-    tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
-
-    # Exclude tables which booking slots is inside requested booking slot
-    tables_booked = Booking.objects.filter(bookingStartDateTime__gt = l_bound_time, bookingEndDateTime__lt = u_bound_time).values('table')
-    tables_booked_ids_temp = [x['table'] for x in tables_booked]
-    tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
-
-    # Exclude tables which include requested booking slot
-    tables_booked = Booking.objects.filter(bookingStartDateTime__lt = l_bound_time, bookingEndDateTime__gt = u_bound_time).values('table')
-    tables_booked_ids_temp = [x['tablee'] for x in tables_booked]
-    tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
-
-    # Then I get a list of all the tables, of the needed size, available in that restaurant and
-    # I exclude the previous list of unavailable tables. I order the list from the smaller table
-    # to the bigger one and I return the first, smaller one, available.
-    tables = Table.objects.filter(maxCapacity__gte=guests).exclude(id__in=tables_booked_ids)
-
-    return tables
--- eResturant_web/app/views/booking.py
+++ eResturant_web/app/views/booking.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from app.models.booking_model import Booking, Table
+from app.models.user_model import userProfile
 from app.forms.booking_forms import InitialBookingForm, editBookingForm, FinalBookingForm
 import datetime
 from django.shortcuts import redirect
 import json
 from json import JSONEncoder
+from django.forms import ValidationError
 
 
 
@@ -13,9 +15,11 @@ from json import JSONEncoder
 
 
 def bookings_view(request, *args, **kwargs):
-    obj = Booking.objects.all()
+    booking = Booking.objects.get(user = request.user)
+    user = userProfile.objects.get(user = request.user)
     my_context = {
-        'object': obj
+        'booking': booking,
+        'user' : user
     }
     return render(request, 'app/bookings.html', my_context)
 
@@ -24,23 +28,30 @@ def booking_create_view(request):
     initial_form = InitialBookingForm(request.POST or None)
     current_user = request.user
     if initial_form.is_valid():
-        f = initial_form.save(commit=False)
-        f.user = current_user
-        f.save()
-        return redirect("bookings/create/table")   
-            
-            
-      
+        if Booking.objects.filter(user = current_user).count() == 0:
+            f = initial_form.save(commit=False)
+            f.user = current_user
+            f.save()
+            return redirect("bookings/create/table") 
+        else:
+          return redirect("bookings/existing")
+
+          
+
     return render(request, 'app/bookings_create.html', { 'initial_form' : initial_form})
 
-def booking_create_success_view(request):
-    return render(request, 'app/bookings_create_successful.html')
 
-
+def booking_existing_view(request):
+    return render(request, 'app/bookings_existing.html')
 
 def booking_create_table_view(request):
     table_form = FinalBookingForm(request.POST or None)    
+    if table_form.is_valid():
+        booking = Booking.objects.all().last()
+        booking.table = table_form.cleaned_data.get('table')
+        booking.save()
 
+        return redirect("bookings")
     return render(request, 'app/bookings_create_table.html', {'table_form' : table_form})
 
 def booking_edit_view(request, *args, **kwargs):
